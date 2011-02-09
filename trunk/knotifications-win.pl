3b4eb2642cd6a02b1ccde3b1bd13ed95785332f3
mrovi@interfete-web-club.com <mrovi@interfete-web-club.com@c3410fac-072f-11df-8158-c7cbc0dd6240>
2011-02-09 10:35:56 +0000

    There's no null in perl

--- trunk/knotifications-win.pl
+++ trunk/knotifications-win.pl
@@ -154,7 +154,7 @@ sub prefs_info_handler {
 sub check_for_updates {
 	my $growl = Purple::Prefs::get_string("/plugins/core/perl_knotifications/growl_command");
 	my $duration = 30;
-	my $icon = null;
+	my $icon = 0;
 	my $version = $PLUGIN_INFO{ 'version' };
 	
 	if (Purple::Prefs::get_bool("/plugins/core/perl_knotifications/show_protocol_icon")) {
@@ -220,7 +220,7 @@ sub get_icon {
 	my ($buddy, $account) = @_;
 
 	if (!Purple::Prefs::get_bool("/plugins/core/perl_knotifications/show_icon")) {
-		return null;
+		return 0;
 	}
 
 	if (Purple::Prefs::get_bool("/plugins/core/perl_knotifications/show_buddy_icon")) {
@@ -243,7 +243,7 @@ sub get_icon {
 		}
 		Purple::Debug::misc("knotifications", "protocol id: $protocol\n");
 
-		my $protocol_icon = null;
+		my $protocol_icon = 0;
 		if ($protocol eq 'prpl-aim') { $protocol_icon = 'aim.png'; }
 		if ($protocol eq 'prpl-bonjour') { $protocol_icon = 'bonjour.png'; }
 		if ($protocol eq 'prpl-gg') { $protocol_icon = 'gadu-gadu.png'; }
@@ -272,7 +272,7 @@ sub get_icon {
 		return $generic;
 	}
 
-	return null;
+	return 0;
 }
 
 sub received_im_msg_handler {
